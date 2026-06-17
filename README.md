8a526f5a6eb18347cb683147fa6a7a1ca67d1360
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-06-17 09:56:17 +0000

    Release 0.0.40

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ Add this to your `Cargo.toml`:
 
 ```toml
 [dependencies]
-yasminaai_api = "0.0.35"
+yasminaai_api = "0.0.40"
 ```
 
 Or install via cargo:
--- src/config.rs
+++ src/config.rs
@@ -31,7 +31,7 @@ impl Default for ClientConfig {
             custom_headers: HashMap::from([
                 ("X-Fern-Language".to_string(), "Rust".to_string()),
                 ("X-Fern-SDK-Name".to_string(), "yasminaai_api".to_string()),
-                ("X-Fern-SDK-Version".to_string(), "0.0.35".to_string()),
+                ("X-Fern-SDK-Version".to_string(), "0.0.40".to_string()),
             ]),
             user_agent: "Api Rust SDK".to_string(),
         }
